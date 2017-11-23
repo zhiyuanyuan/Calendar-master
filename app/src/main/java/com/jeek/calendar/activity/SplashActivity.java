9450722b1a4fee43473d423fb682f21e0d73b414
zhiyuan <zhiyuan.zhang@bolemart.com>
2017-11-23 16:10:44 +0800

    Initial commit

---
+++ app/src/main/java/com/jeek/calendar/activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.jeek.calendar.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by admin on 2017/11/23.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
