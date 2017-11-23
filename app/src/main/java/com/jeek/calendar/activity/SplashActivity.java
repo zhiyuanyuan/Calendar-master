8805dcf53a305482afd1f35fc4e7020d08932633
zhiyuan <zhiyuan.zhang@bolemart.com>
2017-11-23 17:36:56 +0800

    splash页面ui初步搭建

--- app/src/main/java/com/jeek/calendar/activity/SplashActivity.java
+++ app/src/main/java/com/jeek/calendar/activity/SplashActivity.java
@@ -1,17 +1,136 @@
 package com.jeek.calendar.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.jeek.calendar.R;
+import com.jeek.calendar.utils.JellyInterpolator;
 
 /**
  * Created by admin on 2017/11/23.
  */
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends Activity implements View.OnClickListener {
+
+    private TextView mBtnLogin;
+
+    private View progress;
+
+    private View mInputLayout;
+
+    private float mWidth, mHeight;
+
+    private LinearLayout mName, mPsw;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        initView();
+    }
+
+    private void initView() {
+        mBtnLogin = (TextView) findViewById(R.id.main_btn_login);
+        progress = findViewById(R.id.layout_progress);
+        mInputLayout = findViewById(R.id.input_layout);
+        mName = (LinearLayout) findViewById(R.id.input_layout_name);
+        mPsw = (LinearLayout) findViewById(R.id.input_layout_psw);
+
+        mBtnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        mWidth = mBtnLogin.getMeasuredWidth();
+        mHeight = mBtnLogin.getMeasuredHeight();
+
+        mName.setVisibility(View.INVISIBLE);
+        mPsw.setVisibility(View.INVISIBLE);
+
+        inputAnimator(mInputLayout, mWidth, mHeight);
+
+    }
+
+    private void inputAnimator(final View view, float w, float h) {
+
+        AnimatorSet set = new AnimatorSet();
+
+        ValueAnimator animator = ValueAnimator.ofFloat(0, w);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (Float) animation.getAnimatedValue();
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) view
+                        .getLayoutParams();
+                params.leftMargin = (int) value;
+                params.rightMargin = (int) value;
+                view.setLayoutParams(params);
+            }
+        });
+
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mInputLayout,
+                "scaleX", 1f, 0.5f);
+        set.setDuration(1000);
+        set.setInterpolator(new AccelerateDecelerateInterpolator());
+        set.playTogether(animator, animator2);
+        set.start();
+        set.addListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+                progress.setVisibility(View.VISIBLE);
+                progressAnimator(progress);
+                mInputLayout.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+    }
+
+    private void progressAnimator(final View view) {
+        PropertyValuesHolder animator = PropertyValuesHolder.ofFloat("scaleX",
+                0.5f, 1f);
+        PropertyValuesHolder animator2 = PropertyValuesHolder.ofFloat("scaleY",
+                0.5f, 1f);
+        ObjectAnimator animator3 = ObjectAnimator.ofPropertyValuesHolder(view,
+                animator, animator2);
+        animator3.setDuration(1000);
+        animator3.setInterpolator(new JellyInterpolator());
+        animator3.start();
 
     }
 }
+
